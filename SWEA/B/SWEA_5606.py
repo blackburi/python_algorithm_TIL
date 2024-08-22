ae1bda004fcfb94d98e804c1509ab3a309ef5235
김태경 <98coding@gmail.com>
2024-08-22 17:58:29 +0900

    fail to solve

--- SWEA/B/SWEA_5606.py
+++ SWEA/B/SWEA_5606.py
@@ -1,15 +1,37 @@
 # 두 개의 미로
 
 
-from collections import deque
-
 # 방향 설정
 dir = ['N', 'E', 'W', 'S']
+
+# 한명이 못움직이는 경우
+stop = [
+    ('N', 'E'),
+    ('N', 'W'),
+    ('S', 'E'),
+    ('S', 'w'),
+    ('E', 'N'),
+    ('E', 'S'),
+    ('W', 'N'),
+    ('W', 'S'),
+]
 
 T = int(input())
 for tc in range(1, T+1) :
     n = int(input())
 
     # 현민이의 미로 형태
-    hm = deque(list(map(str, input())))
-    print(hm)
+    hm = list(map(str, input()))
+    # 정우의 미로 형태
+    jw = list(map(str, input()))
+
+    # 현민이의 현재 위치 index
+    now_hm = 0
+    # 정우의 현재위치 index
+    now_jw = 0
+
+    while now_hm < n or now_jw < 0 :
+        if hm[now_hm] == jw[now_jw] :
+            now_hm += 1
+            now_jw += 1
+        elif 
