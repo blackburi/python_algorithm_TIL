cf473d7f6aa745a9211ef085b546a92482011c57
김태경 <98coding@gmail.com>
2024-11-01 14:18:46 +0900

    solve : 2_s2_2512.py

--- baekjoon/binary_search/2_s2_2512.py
+++ baekjoon/binary_search/2_s2_2512.py
@@ -3,4 +3,32 @@
 import sys
 input = sys.stdin.readline
 
-# https://www.acmicpc.net/problem/2512
+n = int(input())
+budgets = list(map(int, input().rstrip().split()))
+m = int(input())
+
+answer = 0
+
+bot = 0
+top = max(budgets)
+
+while bot <= top :
+    mid = (bot + top) // 2
+
+    total = 0
+    for budget in budgets :
+        if budget <= mid :
+            total += budget
+        else : # budget > mid
+            total += mid
+
+    if total < m :
+        answer = mid
+        bot = mid + 1
+    elif total > m :
+        top = mid - 1
+    else : # total == m
+        answer = mid
+        break
+
+print(answer)
