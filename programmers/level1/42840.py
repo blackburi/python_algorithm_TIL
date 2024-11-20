835fd253c1059f969066c9d64cdc5f78d8bf6c80
김태경 <98coding@gmail.com>
2024-11-20 16:21:50 +0900

    solve: programmers 42840.py

---
+++ programmers/level1/42840.py
@@ -0,0 +1,26 @@
+def solution(answers):
+    answer = []
+    
+    # 정답을 맞춘 갯수
+    sol = [0, 0, 0]
+    
+    one = [1, 2, 3, 4, 5] * 2000
+    two = [2, 1, 2, 3, 2, 4, 2, 5] * 1250
+    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
+    
+    for i in range(len(answers)) :
+        ans = answers[i]
+        if one[i] == ans :
+            sol[0] += 1
+        if two[i] == ans :
+            sol[1] += 1
+        if three[i] == ans :
+            sol[2] += 1
+            
+    M = max(sol)
+    
+    for i in range(3) :
+        if M == sol[i] :
+            answer.append(i+1)
+    
+    return answer
