6c25b527a121d75ea67fe20514d23934094469e0
김태경 <98coding@gmail.com>
2025-04-16 23:32:39 +0900

    fail : 3_g5_20208

--- baekjoon/backtracking/3_g5_20208.py
+++ baekjoon/backtracking/3_g5_20208.py
@@ -4,46 +4,50 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
+# n*n, 기초체력, 증가량
 n, m, h = map(int, input().split())
 
-# 우유가 있는 집
-houses = deque()
+# 집
+home_x = -1
+home_y = -1
+# 민트 초코 위치
+mints = []
+# 마실 수 있는 최대 민트초코
+ans = 0
 
 for i in range(n) :
-    sub = list(input().rstrip().split())
+    row = list(map(int, input().split()))
     for j in range(n) :
-        # 진우의 집
-        if sub[j] == '1' :
-            start = (i, j)
-            houses.appendleft((i, j))
-        # 우유가 있는 집
-        if sub[j] == '2' :
-            houses.append((i, j))
+        if row[j] == 1 :
+            home_x = i
+            home_y = j
 
-# 방문 처리 / index=0 최종 도착지(진우집)
-visited = {}
-for i in range(len(houses)) :
-    visited[i] = False
+        if row[j] == 2 :
+            # x, y, 방문 check
+            mints.append([i, j, 0])
+print(mints)
+# (x, y), 체력, 민트초코의 수
+def dfs(x:int, y:int, stamina:int, cnt:int) :
+    global ans
 
-# 진우가 먹는 우유의 최대 개수
-cnt = 0
-
-
-# 현재 위치, 먹은 우유의 개수, 남은 체력
-def dfs(x, y, milk, stamina) :
-    global cnt
-
-    if milk != 0 and (x, y) == start :
-        cnt = max(cnt, milk-1)
+    # 집으로 갈 수 있는 경우
+    if x == home_x and y == home_y :
+        ans = max(ans, cnt)
         return
+    
+    for mint in mints :
+        # 방문하지 않고, 갈 수 있는 민트 초코
+        if mint[2] == 0 and abs(mint[0]-x)+abs(mint[1]-y) <= stamina :
+            # 방문하는 경우
+            mint[2] = 1
+            new_stamina = stamina + h - (abs(mint[0]-x)+abs(mint[1]-y))
+            # stamina는 초기 체력 이상으로 올라갈 수 없음
+            new_stamina = min(m, new_stamina)
+            dfs(mint[0], mint[1], new_stamina, cnt+1)
+            # 방문하지 않는 경우
+            mint[2] = 0
+            continue
 
-    for i in range(len(visited)) :
-        if (visited[i] is False) and (stamina >= abs(x-houses[i][0])+abs(y-houses[i][1])) :
-            visited[i] = True
-            dfs(houses[i][0], houses[i][1], milk+1, stamina-(abs(x-houses[i][0])+abs(y-houses[i][1]))+h)
-            visited[i] = False
 
-
-dfs(start[0], start[1], 0, m)
-
-print(cnt)
+dfs(home_x, home_y, m, 0)
+print(ans)
