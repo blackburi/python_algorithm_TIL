0d66ad7e0c061de4fcb5fb7e385200da8246400c
김태경 <98coding@gmail.com>
2025-05-06 04:14:03 +0900

    solve : 2_s2_11724.py

--- baekjoon/Union_Find/2_s2_11724.py
+++ baekjoon/Union_Find/2_s2_11724.py
@@ -6,10 +6,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+# 대표 node 설정
+parents = [i for i in range(n+1)]
+
+
 def find(x) :
     if parents[x] != x :
-        return find(parents[x])
-    return x
+        parents[x] = find(parents[x])
+    return parents[x]
 
 def union(x, y) :
     x = find(x)
@@ -21,13 +25,13 @@ def union(x, y) :
     else : # x <= y
         parents[y] = x
 
-
-# 대표 node 설정
-parents = [i for i in range(n+1)]
-
 for _ in range(m) :
     a, b = map(int, input().split())
-    union(a, b)
+    if find(a) != find(b) :
+        union(a, b)
+
+for i in range(1, n+1) :
+    find(i)
 
 parents = set(parents)
 # 0 제외
